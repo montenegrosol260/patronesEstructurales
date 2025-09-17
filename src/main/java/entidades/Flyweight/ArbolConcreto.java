8274d561487cb567f10cd56350106342abd88a32
montenegrosol260 <montenegrosol260@gmail.com>
2025-09-17 11:06:05 -0300

    1

--- src/main/java/entidades/Flyweight/ArbolConcreto.java
+++ src/main/java/entidades/Flyweight/ArbolConcreto.java
@@ -1,12 +1,22 @@
 package entidades.Flyweight;
 
 public class ArbolConcreto implements Árbol{
+    //estados intrínsicos
     private String tipoArbol;
     private String textura;
     private String color;
 
-    @Override
-    public void dibujarArbol() {
+    public ArbolConcreto(String tipoArbol, String textura, String color) {
+        this.tipoArbol = tipoArbol;
+        this.textura = textura;
+        this.color = color;
+        System.out.println("El árbol de "+ tipoArbol + ", de color "+ color + " y textura "
+                + textura + "fue creado correctamente");
+    }
 
+    @Override
+    public void dibujarArbol(int x, int y) {
+        System.out.println( "El árbol de "+ tipoArbol + ", de color "+ color + " y textura "
+                + textura + "fue dibujado correctamente en la posición " + x + ", " + y);
     }
 }
--- src/main/java/entidades/Flyweight/Árbol.java
+++ src/main/java/entidades/Flyweight/Árbol.java
@@ -1,5 +1,5 @@
 package entidades.Flyweight;
 
 public interface Árbol {
-    public void dibujarArbol();
+    public void dibujarArbol(int x, int y); //estado exintrínsico posición
 }
