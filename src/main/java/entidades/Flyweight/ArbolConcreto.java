57d8927f67cc2e9c4593e38401e21713819c59d8
montenegrosol260 <montenegrosol260@gmail.com>
2025-09-17 10:58:48 -0300

    1

---
+++ src/main/java/entidades/Flyweight/ArbolConcreto.java
@@ -0,0 +1,12 @@
+package entidades.Flyweight;
+
+public class ArbolConcreto implements Árbol{
+    private String tipoArbol;
+    private String textura;
+    private String color;
+
+    @Override
+    public void dibujarArbol() {
+
+    }
+}
---
+++ src/main/java/entidades/Flyweight/Árbol.java
@@ -0,0 +1,5 @@
+package entidades.Flyweight;
+
+public interface Árbol {
+    public void dibujarArbol();
+}
